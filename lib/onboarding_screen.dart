702d91c91c079820633021b3a92b04f525a93dc8
Lukas Steiner <lsteiner002@drury.edu>
2025-10-04 23:28:55 +0200

    Initial commit

---
+++ lib/onboarding_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:test/components/onboard_content.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  late PageController _onboardPageController;
+
+  @override
+  void initState() {
+    _onboardPageController = PageController()
+      ..addListener(() {
+        setState(() {});
+      });
+    super.initState();
+    Future.delayed(Duration.zero, () {
+      showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(42),
+            topRight: Radius.circular(42),
+          ),
+        ),
+        builder: (_) => const OnboardContent(),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+      ),
+      body: Image.asset(
+        "assets/bg.png",
+        width: double.infinity,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
